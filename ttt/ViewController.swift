bebc3f5972418a57ed7cd27d139afbd397f06bc6
ryo <fumiki.kyohei@gmail.com>
2024-02-06 17:55:25 +0900

    Initial Commit

---
+++ ttt/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ttt
+//
+//  Created by 鈴木亮平 on 2024/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    aaaaaaaaa
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tttUITests/tttUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tttUITestsLaunchTests.swift
+//  tttUITests
+//
+//  Created by 鈴木亮平 on 2024/02/06.
+//
+
+import XCTest
+
+final class tttUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
